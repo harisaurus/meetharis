cebf3d3e7d81b27b1a0377f1908d0f2fce7d7e3d
Haris Mahmood <harismahmood89@gmail.com>
2020-06-05 13:28:07 -0400

    all data loading

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,85 @@
 import { useEffect, useState } from "react";
 import Head from "next/head";
-// import Post from "../components/post";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 const client = require("contentful").createClient({
   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
   accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
 });
 
-function HomePage() {
-  async function fetchEntries() {
-    const entries = await client.getEntries();
-    if (entries.items) return entries.items;
-    console.log(`Error getting Entries for ${contentType.name}.`);
+const HomePage = () => {
+  async function fetchEntry(entryId) {
+    const entry = await client.getEntry(entryId);
+    if (entry.fields) return entry.fields;
+    console.log(`Error getting Entry with id ${entryId}`);
   }
 
-  const [posts, setPosts] = useState([]);
+  async function fetchEntries(entryType) {
+    const entries = await client.getEntries({
+      content_type: entryType,
+      order: "-fields.date",
+    });
+    if (entries.items) return entries.items;
+    console.log(`Error getting Entries for ${entryType}`);
+  }
+
+  const [main, setMain] = useState();
+  const [talks, setTalks] = useState([]);
 
   useEffect(() => {
-    async function getPosts() {
-      const allPosts = await fetchEntries();
-      setPosts([...allPosts]);
-      console.log("posts", posts);
+    async function getTalkEntries() {
+      const talks = await fetchEntries("talk");
+      setTalks(talks);
     }
-    getPosts();
+
+    getTalkEntries();
   }, []);
+
+  useEffect(() => {
+    async function getMainEntry() {
+      const mainEntry = await fetchEntry("55EnWlaqDrfmJAs9imUnp6");
+      console.log(mainEntry);
+      setMain(mainEntry);
+    }
+
+    getMainEntry();
+  }, []);
+
+  if (!main) {
+    return (
+      <>
+        <h1>loading...</h1>
+      </>
+    );
+  }
 
   return (
     <>
       <Head>
-        <title>Next.js + Contentful</title>
+        <title>Haris Mahmood</title>
         <link
           rel="stylesheet"
           href="https://css.zeit.sh/v1.css"
           type="text/css"
         />
       </Head>
-      <div>
-        <h1>Meet Haris</h1>
-      </div>
-      {/* {posts.length > 0
-        ? posts.map(p => (
-            <Post
-              alt={p.fields.alt}
-              date={p.fields.date}
-              key={p.fields.title}
-              image={p.fields.image}
-              title={p.fields.title}
-              url={p.fields.url}
-            />
+      <div>{documentToReactComponents(main.content)}</div>
+
+      <div>{main.ctaText}</div>
+
+      {talks.length > 0
+        ? talks.map((t, tIndex) => (
+            <li key={tIndex}>
+              <span>{t.fields.event}</span>
+              <h3>{t.fields.name}</h3>
+              <span>{t.fields.friendlyDate}</span>
+            </li>
           ))
-        : null} */}
+        : null}
+
+      <div>{main.footer}</div>
     </>
   );
-}
+};
 
 export default HomePage;

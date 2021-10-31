b236636f700bf3feb02e942c1a824620e8ab1e0b
Haris Mahmood <harismahmood89@gmail.com>
2021-10-31 13:37:12 -0400

    upgrade to next v12

--- next.config.js
+++ next.config.js
@@ -1,2 +1,7 @@
-const withFonts = require("next-fonts");
-module.exports = withFonts();
+// const withFonts = require("next-fonts");
+// module.exports = withFonts();
+
+
+module.exports = {
+  swcMinify: true,
+}
